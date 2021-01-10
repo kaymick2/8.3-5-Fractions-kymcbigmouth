72c8912d1b998fc2ee244ef61aef2ea3dc1e166f
Kyle McDowell <kymcd21@icstudents.org>
2021-01-10 04:35:58 +0000

    BIG BRAIN

--- bigMath.java
+++ bigMath.java
@@ -5,14 +5,16 @@ public class bigMath {
   private int n2;
   private int d1;
   private int d2;
-  private int ans;
+  private String ans;
+  private int gcf;
 
-  public bigMath(int n1, int d1, int n2, int d2) {
+  public bigMath(int n1, int d1, int n2, int d2, String ans,int gcf) {
     this.n1 = n1;
     this.n2 = n2;
     this.d1 = d1;
     this.d2 = d2;
     this.ans = ans;
+    this.gcf=gcf;
 
   }
 
@@ -27,7 +29,7 @@ public class bigMath {
   public String subtraction(int n1, int d1, int n2, int d2) {
     int numans = ((n1 * d2) - (n2 * d1));
     int denans = (d1 * d2);
-    String ans = simping(numans, denans, realAns);
+    String ans = simping(numans, denans);
 
     return ans;
     // calls x and z, defines b as x - z and tells the program to return b if called
@@ -36,7 +38,7 @@ public class bigMath {
   public String multiplication(int n1, int d1, int n2, int d2) {
     int numans = (n1 * n2);
     int denans = (d1 * d2);
-    String ans = simping(numans, denans, realAns);
+    String ans = simping(numans, denans);
 
     return ans;
     // calls x and z, defines c as x*z, and puts c to be returned if called
@@ -45,7 +47,7 @@ public class bigMath {
   public String division(int n1, int d1, int n2, int d2) {
     int numans = (n1 * d2);
     int denans = (d1 * n2);
-    String ans = simping(numans, denans, realAns);
+    String ans = simping(numans, denans);
 
     return ans;
     // calls x and z, defines d as x/z, and puts d to be returned if method is
@@ -54,17 +56,17 @@ public class bigMath {
     // of like a function. return d; says that if you put x and z in, you get d out.
   }
 
-  public String simping(int numans, int denans, String realAns) {
-    int gcf;
+  public String simping(int numans, int denans) {
+    int gcf=0;
     for (int i = 1; i <= numans && i <= denans; i++) {
-      if (numans % i == 0 && denans % i == 0) {
+      if (numans % i == 0 && denans % i == 0){
 
-        gcf = i;
-      }
-
+      
+        gcf = i;}
+        
     }
-    realAns = (numans / gcf) + "/" + (denans / gcf);
-    return realans;
+  String realAns= (numans/gcf)+"/"+(denans/gcf);
+    return realAns;
   }
 
 }
